9e871f9c985be77e645f1a301304faae4871e465
klwork <klwork-1234@yahoo.com.cn>
2013-07-17 18:38:15 +0800

    把权限改的清晰，解决谷歌浏览器的登录问题

--- src/main/java/com/klwork/explorer/web/LoginController.java
+++ src/main/java/com/klwork/explorer/web/LoginController.java
@@ -12,28 +12,39 @@
  */
 package com.klwork.explorer.web;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.LockedAccountException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * The Class LoginController.
  */
 @Controller
-@RequestMapping("/")
+@RequestMapping("*")
 public class LoginController
 {
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(value = "login", method = RequestMethod.GET)
     public String login(HttpServletRequest request)
     {
     	System.out.println("login:" + request.getRequestURL());
         return "login";
     }
 
-    @RequestMapping(value = "/", method = RequestMethod.POST)
+    @RequestMapping(value = "login", method = RequestMethod.POST)
     public String loginForm(HttpServletRequest request, ModelMap modelMap)
     {
         if (request.getAttribute("shiroLoginFailure") != null)
@@ -55,4 +66,45 @@ public class LoginController
 
         return "login";
     }
+    
+    @RequestMapping(value = "login-submit")
+    public void loginSubmit(@RequestParam("username") String username,  
+            @RequestParam("password") String password,HttpServletRequest request,HttpServletResponse response) throws IOException {  
+  
+        Subject currentUser = SecurityUtils.getSubject();  
+        String result = "login";  
+        if (!currentUser.isAuthenticated()) {  
+            result = login(currentUser,username,password); 
+            //return new ModelAndView(result);
+        }else{//重复登录  
+           /* ShiroUser shiroUser = (ShiroUser) currentUser.getPrincipal();  
+            if(!shiroUser.getLoginName().equalsIgnoreCase(username)){//如果登录名不同  
+                currentUser.logout();  
+                result = login(currentUser,username,password);  
+            }  */
+        }  
+        String index = request.getContextPath() + "/";
+		response.sendRedirect(index);
+    }
+      
+    private String login(Subject currentUser,String username,String password){  
+        String result = "login";  
+        UsernamePasswordToken token = new UsernamePasswordToken(username,  
+                password);  
+        token.setRememberMe(false);  
+        try {  
+            currentUser.login(token);  
+            result = "success";  
+        } catch (UnknownAccountException uae) {  
+            result = "failure";  
+        } catch (IncorrectCredentialsException ice) {  
+            result = "failure";  
+        } catch (LockedAccountException lae) {  
+            result = "failure";  
+        } catch (AuthenticationException ae) {  
+            result = "failure";  
+        }  
+        return result;  
+    }  
+    
 }
